502be0d5c8657d86b2e9dfedf8cee3ce1ba814e1
nguspkt.it.1101 <nguspkt.it.1101@gmail.com>
2014-09-11 04:51:52 +0000

    update

--- Hello_World_1/src/mypack/HelloWorld.java
+++ Hello_World_1/src/mypack/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("HelloWorld");
+		System.out.print("HelloWorld 123");
 	}
 
 }
